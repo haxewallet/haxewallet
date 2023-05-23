6186d2b3850a1f06f41db66c83fe399d62e381f0
nonlin-lin-chaos-order-etc-etal <hypnosis.i2p@gmail.com>
2023-05-23 12:01:38 +0800

    Update design

--- src/MainWindow.hx
+++ src/MainWindow.hx
@@ -9,7 +9,7 @@ import haxe.ui.containers.windows.Window;
         <grid id="contentPane" width="100%">
         </grid>
         <window-footer width="100%">
-            <dropdown width="100">
+            <!--<dropdown width="100">
                 <data>
                     <item text="Item 1" />
                     <item text="Item 2" />
@@ -21,10 +21,11 @@ import haxe.ui.containers.windows.Window;
                     <item text="Item 8" />
                     <item text="Item 9" />
                 </data>
-            </dropdown>
-            <spacer width="100%" />
-            <button text="Action1" />
-            <button text="Action2" />
+            </dropdown>-->
+            <spacer width="50%" />
+            <button text="Create new wallet" />
+            <button text="Import wallet" />
+            <spacer width="50%" />
         </window-footer>
     </window>
 ')
